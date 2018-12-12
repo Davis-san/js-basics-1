6ddea6b1af6e03cfad3566755574b2e95e5d7e5e
Davis-san <kalai808linda@gmail.com>
2018-12-11 17:46:15 -1000

    sorry for the delay

--- basics.js
+++ basics.js
@@ -1,22 +1,87 @@
 /* Create a `myName` variable and assign it a String value */
+var word = ('myName');
 
 /* Create a `person` variable and give it 2 properties,
  * `name`, assign it the same name as before,
  * as well as an `age` (number);
  */
 
+   
+var person = {
+    firstName: "Sam",
+    lastName : "Joe",
+    age     : "30",
+
+}
+
+
 /* Create a variable called `canDrive`,
  * if it should be true if your person object is at least 16 years old
  */
+
+
+var canDrive = true;
+if(canDrive){
+  console.log('canDrive');
+} else {
+  console.log('no can drive....);
+}
+
+
+
 
 /* Create a function called `greet`,
  * it should take a 1 parameter, `name`
  * and it should print "Hello, my name is {name}"
  */
 
+
+//i need help with this one....
+
+  function myFunction(greet) {
+    return hello
+  }
+
+
+
+
 /* Create an array called `dataTypes` with atleast 1 of every data type;
  * (there are 6 different data types);
  */
+
+Boolean
+Number
+String
+Null
+Undefined
+Symbol
+
+//boolean
+var happy = true;
+var mad = false;
+
+//number
+var num = 123
+var numb = -Infinity
+
+//stirng
+var word1 = 'hello my name is davis'
+var word2 = 'hello i said my name is davis'
+
+//null
+var nothing = null
+
+//undefined 
+var favoriteColor = Red;
+console.log(and blue)
+
+//symbol
+const symbol = symbol;
+
+
+
+
+
 
 /* Create a `dog` object
  * it should have a `bark` function that makes your dog bark!
